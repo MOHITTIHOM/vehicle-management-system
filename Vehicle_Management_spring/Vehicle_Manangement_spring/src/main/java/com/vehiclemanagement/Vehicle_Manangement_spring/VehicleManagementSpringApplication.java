c7e0f3aa6f66cd8b845bae28bbe2967c26623113
MOHITTIHOM <mohitbhandari.1995@gmail.com>
2024-08-29 13:22:59 +0530

    Added System.out.println('hello world') for testing purpose

--- Vehicle_Management_spring/Vehicle_Manangement_spring/src/main/java/com/vehiclemanagement/Vehicle_Manangement_spring/VehicleManagementSpringApplication.java
+++ Vehicle_Management_spring/Vehicle_Manangement_spring/src/main/java/com/vehiclemanagement/Vehicle_Manangement_spring/VehicleManagementSpringApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class VehicleManagementSpringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello, I am working");
 		SpringApplication.run(VehicleManagementSpringApplication.class, args);
 	}
 
