19f57003eb02cf6ccba25d771f14ab57d819f650
MOHITTIHOM <mohitbhandari.1995@gmail.com>
2024-08-29 14:08:01 +0530

    Remove sout for workflow test

--- Vehicle_Management_spring/Vehicle_Manangement_spring/src/main/java/com/vehiclemanagement/Vehicle_Manangement_spring/VehicleManagementSpringApplication.java
+++ Vehicle_Management_spring/Vehicle_Manangement_spring/src/main/java/com/vehiclemanagement/Vehicle_Manangement_spring/VehicleManagementSpringApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class VehicleManagementSpringApplication {
 
 	public static void main(String[] args) {
-		System.out.println("First");
 		SpringApplication.run(VehicleManagementSpringApplication.class, args);
 	}
 
